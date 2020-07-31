0411ef33cd3d059a9a0487222fc6d5332d336f9b
DeAdFrOnt <semenukmaks@gmail.com>
2020-07-31 20:40:26 +0300

    update package.json

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import { hot } from 'react-hot-loader'
+
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+class App extends React.Component {
+    render() {
+        return <div>Hello {this.props.name}</div>;
+    }
+}
+
+const AppWithHot = hot(module)(App)
+
+var mountNode = document.getElementById('app')
+ReactDom.render(<AppWithHot name="Jane" />, mountNode)
